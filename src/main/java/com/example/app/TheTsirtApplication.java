66361f7d7a3938315c7654fd24f7ded535643eb8
leonx04 <jungnguyen2108@gmail.com>
2024-07-10 02:27:05 +0700

    chore: Add initial project files and configuration

---
+++ src/main/java/com/example/app/TheTsirtApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTsirtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheTsirtApplication.class, args);
+    }
+
+}
